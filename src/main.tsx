d3b59568195f592e46bc840266f2f425e7785a48
WMB Alex <alex@worldmediabuy.com>
2025-03-30 23:09:11 +0300

    Update manifest and configuration

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-// this manifest is used temporarily for development purposes
-const manifestUrl =
-  "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
+
+// Using the full GitHub Pages URL for the manifest
+const manifestUrl = "https://wmbalex.github.io/twa-ton/tonconnect-manifest.json";
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
